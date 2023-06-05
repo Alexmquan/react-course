9d98b7d59a937dbdd721485775da093d6e29f39e
alexmquan <alexmquan93@gmail.com>
2023-06-05 16:17:36 -0600

    Reset app to base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-import Modal from "./components/Modal.js";
-import Todo from "./components/Todo.js";
-import Backdrop from "./components/Backdrop.js";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Get interview" />
-      <Todo text="acquire job" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
