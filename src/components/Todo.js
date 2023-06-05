2c74b208983af64209e05f25309aa936e6f08dc3
alexmquan <alexmquan93@gmail.com>
2023-06-05 16:12:21 -0600

    Made state of backdrop reactive

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,6 +9,10 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -17,9 +21,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
-      {modalIsOpen ? <Modal /> : null}
-
-      <Backdrop />
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
