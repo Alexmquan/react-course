a3e864c43d6e05272801dfea6e358f235675bf59
alexmquan <alexmquan93@gmail.com>
2023-06-05 16:44:45 -0600

    Add link capabilities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups.js";
+import NewMeetupPage from "./pages/NewMeetup.js";
+import FavoritesPage from "./pages/Favorites.js";
+import MainNavigation from "./components/layout/MainNavigation.js";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
