2443120e914c39a992e0578aa37f3f62775c0472
alexmquan <alexmquan93@gmail.com>
2023-06-06 11:24:40 -0600

    built favorites badge

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorites-context.js";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -11,7 +15,12 @@ function MainNavigation() {
             <Link to="/">All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup">Add New Meetup</Link>
+            <Link to="/new-meetup">
+              Add New Meetup
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
           <li>
             <Link to="/favorites">Favorites</Link>
