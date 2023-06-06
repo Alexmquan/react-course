9d4e1853eff03f46bf31eec561b05a53dcf549d7
alexmquan <alexmquan93@gmail.com>
2023-06-06 10:53:53 -0600

    Created context and wrapped app with it

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.js";
 import "./index.css";
 import App from "./App";
+import { FavoritesContextProvider } from "./store/favorites-context.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>
 );
